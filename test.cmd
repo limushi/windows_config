7b96c264a8eac50b65743d4364f4c12d08f835f1
Maxim Lianov <maxlianov@gmail.com>
2021-06-23 23:53:29 +0300

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -4,3 +4,13 @@ rem automatically installs the software
 
 echo on
 echo "begin for config your host"
+
+rem curl_src="https://curl.se/windows/dl-7.77.0_2/curl-7.77.0_2-win64-mingw.zip"
+set curl_src="https://curl.se/windows/dl-7.77.0_2/curl-7.77.0_2-win64-mingw.zip"
+echo %curl_src%
+
+rem set dest for this jobs
+rem dir_path="c:\"
+set dir_path="c:\"
+echo %dir_path%
+
